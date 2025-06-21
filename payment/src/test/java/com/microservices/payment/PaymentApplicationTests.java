cd19f2bf5a89bf96706ac82a99759db15049a6b4
Mohammed CHBANI <mohamedachbani5@gmail.com>
2025-06-21 02:17:18 +0100

    fixing properties

--- payment/src/test/java/com/microservices/payment/PaymentApplicationTests.java
+++ payment/src/test/java/com/microservices/payment/PaymentApplicationTests.java
@@ -2,9 +2,17 @@ package com.microservices.payment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class PaymentApplicationTests {
+
+	@MockitoBean
+	ClientRegistrationRepository ClientRegistrationRepository;
+	@MockitoBean
+	OAuth2AuthorizedClientRepository OAuth2AuthorizedClientRepository;
 
 	@Test
 	void contextLoads() {
